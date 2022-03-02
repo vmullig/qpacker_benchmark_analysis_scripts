62e426977c5bd9b56fff017ed3a8104a47ead584
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-03-02 15:34:19 -0500

    Script to extract data to plot.

---
+++ extract_data_to_plot.sh
@@ -0,0 +1,16 @@
+echo "Solution_space_size QPacker_exptime Toulbar_time" > summary.txt
+echo "Solution_space_size QPacker_exptime Toulbar_time" > summary_lowestE.txt
+echo "Solution_space_size QPacker_exptime Toulbar_time" > summary_not_lowestE.txt
+for f in `find . -name analysis.log`
+do
+    solsize=`grep "Solution space size" $f | awk '{print $4}'`
+    test "$solsize" == "" && continue
+    ttime=`grep "Toulbar2 time" $f | awk '{print $4}'`
+    test "$ttime" == "" && continue
+    qtime=`grep "Expectation time" $f | awk '{print $8}'`
+    test "$qtime" == "" && continue
+    bestislowest=`grep "QPacker best is Toulbar2 lowest energy" $f | awk '{print $7}'`
+    echo "$solsize $ttime $qtime $bestislowest" >> summary.txt
+    test "$bestislowest" == "TRUE" && echo "$solsize $ttime $qtime $bestislowest" >> summary_lowestE.txt || echo "$solsize $ttime $qtime $bestislowest" >> summary_not_lowestE.txt
+    echo "Completed $f."
+done
