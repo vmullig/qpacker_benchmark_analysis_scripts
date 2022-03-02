947af8db3d982c891f73a30eedf73f59647b4f14
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2022-03-02 16:42:54 -0500

    Adding line of best fit.

--- plot_data.py
+++ plot_data.py
@@ -1,8 +1,13 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.optimize as opt
+import math
 
 matplotlib.use('TkAgg')
+
+def fit_func(x,m,b):
+    return m*x+b
 
 def read_file( filename ) :
     with open( filename ) as filehandle:
@@ -40,4 +45,16 @@ plt.yscale('log')
 plt.xscale('log')
 plt.xlabel( "Size of solution space\n(number of possible solutions)" )
 plt.ylabel( r"Average time to find lowest-energy solution ($\mu$s)" )
+
+# Linear fit:
+toulbar2_times_all_uncertainty = toulbar2_times_all**2
+toulbar2_fit, toulbar2_cov = opt.curve_fit( fit_func, probsizes_all, toulbar2_times_all, [1, 1], sigma=toulbar2_times_all_uncertainty, absolute_sigma=True )
+qpacker_times_solved_uncertainty = qpacker_times_solved**2
+qpacker_solved_fit, qpacker_solved_cov = opt.curve_fit( fit_func, probsizes_solved, qpacker_times_solved, [1, 1], sigma=qpacker_times_solved_uncertainty, absolute_sigma=True )
+plotrange_all = np.logspace( math.log(min(probsizes_all), 10), math.log(max(probsizes_all), 10), 50, dtype=np.float64, base=10 )
+print( plotrange_all )
+plotrange_solved = np.logspace( math.log(min(probsizes_solved), 10), math.log(max(probsizes_solved), 10), 50, dtype=np.float64, base=10 )
+plt.plot( plotrange_all, fit_func( plotrange_all, *toulbar2_fit ), '--', c='blue', linewidth=2 )
+plt.plot( plotrange_solved, fit_func( plotrange_solved, *qpacker_solved_fit ), '--', c='red', linewidth=2 )
+
 plt.show()
